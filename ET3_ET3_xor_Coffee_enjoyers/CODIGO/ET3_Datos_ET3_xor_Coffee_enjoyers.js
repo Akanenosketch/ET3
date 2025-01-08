82e4780b4d0537a96497d1f401b4c98f0b8d3e9d
Aka <90091466+Akanenosketch@users.noreply.github.com>
2025-01-08 20:37:30 +0100

    Horas mías y de Manu puestas

--- ET3_ET3_xor_Coffee_enjoyers/CODIGO/ET3_Datos_ET3_xor_Coffee_enjoyers.js
+++ ET3_ET3_xor_Coffee_enjoyers/CODIGO/ET3_Datos_ET3_xor_Coffee_enjoyers.js
@@ -8,6 +8,6 @@ horas totales
 def_grupo_ET3_xor_Coffee_enjoyers = Array("ET3",
     "ET3_xor_Coffee_enjoyers",
     Array("Torrecilla Rio Manuel Leonardo", "Rodriguez Lorenzo Daniel Fabian", "Quintas Rodriguez Tatiana Maria", "Simon Novoa David"),
-    Array(0, 37, 0, 0),
+    Array(32, 37, 35, 0),
     37
 );
