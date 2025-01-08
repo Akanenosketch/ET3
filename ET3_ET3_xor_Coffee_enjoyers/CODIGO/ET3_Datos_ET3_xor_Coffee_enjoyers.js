46614e6164cee70f53799cbd623885998f986339
Aka <90091466+Akanenosketch@users.noreply.github.com>
2025-01-08 20:39:36 +0100

    Se me olvidó de sumar las horas lol

--- ET3_ET3_xor_Coffee_enjoyers/CODIGO/ET3_Datos_ET3_xor_Coffee_enjoyers.js
+++ ET3_ET3_xor_Coffee_enjoyers/CODIGO/ET3_Datos_ET3_xor_Coffee_enjoyers.js
@@ -9,5 +9,5 @@ def_grupo_ET3_xor_Coffee_enjoyers = Array("ET3",
     "ET3_xor_Coffee_enjoyers",
     Array("Torrecilla Rio Manuel Leonardo", "Rodriguez Lorenzo Daniel Fabian", "Quintas Rodriguez Tatiana Maria", "Simon Novoa David"),
     Array(32, 37, 35, 0),
-    37
+    104
 );
